90c05567df94e0b539c83eb7110556c45259501d
jimieloranta <jeloranta70@gmail.com>
2022-08-24 11:36:59 +0300

    Tiedoston muutos ja tekstin vaihto

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,13 +1,31 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getOstoslista } from "~/api.server";
+
+export const loader = async () => {
+  const ostoslista = await getOstoslista();
+  return json(ostoslista);
+};
+
 export default function Index() {
+  const ostoslista = useLoaderData();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Funikulaarin ja logomon sillan hinta Bitcoineina</h1>
-      <p>
-        Funikulaarin hinta 260.56 ₿
-      </p>
-      <p>
-        Logomon sillan hinta 1005.78 ₿
-      </p>
+      <h1>Funikulaarin ja Logomon sillan hinta Bitcoineina</h1>
+      <ul>
+        {ostoslista.map((ostos, index) => {
+          const nimi = ostos.nimi;
+          if (ostos.hankittu) {
+            return (
+              <li key={index}>
+                <s>{nimi}</s>
+              </li>
+            );
+          }
+          return <li key={index}>{nimi}</li>;
+        })}
+      </ul>
     </div>
   );
 }
